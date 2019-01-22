7b2ea921ecf56cdaebced1d58e3baee246c00baf
rmejia-tajc <rmejia.tajc@gmail.com>
2019-01-22 14:54:58 -0700

    finish MVP. Refactor so code is DRY

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
+import MovieCard from './MovieCard';
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -33,23 +35,14 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
+  const { id } = movie;
   return (
-    <Link to={`/movies/${movie.id}`} className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
+    <Link to={`/movies/${id}`} >
+
+      <MovieCard movie={movie} />
+
     </Link>
+    
   );
 }
